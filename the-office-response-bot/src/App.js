1595554bd69445ec8783dac2228f66fd2885b68a
Zuby Javed <zuby@zubys-mbp.lan>
2022-02-04 15:23:17 -0500

    return random office line to user

--- the-office-response-bot/src/App.js
+++ the-office-response-bot/src/App.js
@@ -1,19 +1,25 @@
 import React, { useEffect, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import data from './office-lines.js';
+import FuzzySet from "fuzzyset.js";
+import { flushSync } from "react-dom";
 
 function App() {
 
-  // the line displayed to user when they hit enter. 
+  const [lines, setLines] = useState(data);
   const [userText, setUserText] = useState('');
-  // the bots state is updated with user text. 
   let michaelLine = 'Sometimes I’ll start a sentence and I don’t even know where it’s going. I just hope I find it along the way.';
   const [botResponse, setBotResponse] = useState(michaelLine);
 
+  const getResponse = () => {
+    let randomIndex = Math.floor(Math.random() * lines.length-1);
+    setBotResponse(lines[randomIndex]);
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    setBotResponse(userText); 
-    console.log('hello world');
+    getResponse();
   }
 
   return (

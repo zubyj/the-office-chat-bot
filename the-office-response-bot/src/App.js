43c06e71299f76b7f33f5830d5e15d5e1a0bb007
Zuby Javed <zuby@zubys-mbp.lan>
2022-02-06 00:19:59 -0500

    toggle mic for text to speech

--- the-office-response-bot/src/App.js
+++ the-office-response-bot/src/App.js
@@ -1,5 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import logo from './prison-mike.jpeg';
+import on_mic from './mic.svg';
+import off_mic from './muted_mic.svg'
 import './App.css';
 import data from './office-lines.js';
 import FuzzySet from "fuzzyset.js";
@@ -12,6 +14,8 @@ function App() {
   let michaelLine = 'Sometimes I’ll start a sentence and I don’t even know where it’s going. I just hope I find it along the way.';
   const [botResponse, setBotResponse] = useState(michaelLine);
 
+  const [mute, setMute] = useState(false);
+  
   // gets all lines from the office.
   useEffect(() => {
     const getLines = () => {
@@ -28,28 +32,48 @@ function App() {
   
   // find closest matching line to user text
   // respond to user w/ next line in the show. 
-  // return the next line in the show. 
   const handleSubmit = (e) => {
     e.preventDefault();
-    let score = lines.get(userText);
-    let best_match_line = score[0][1];
-    let index = data.indexOf(best_match_line);
+    let scores = lines.get(userText);
+    let best_score = scores[0][1];
+    let index = data.indexOf(best_score);
     let response = data[index+1];
     setBotResponse(response);
+
+    if (!mute) {
+      let message = new SpeechSynthesisUtterance();
+      message.text = response;
+      window.speechSynthesis.speak(message);
+    }
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          The Office Response Bot
-        </p>
+        <h3>The Office Response Bot</h3>
         <form onSubmit={handleSubmit}>
           <input type="text" name="userLine" onChange={(e) => setUserText(e.target.value)}/>
-          <button type="submit">
-            Submit
-          </button>
+          <button type="submit">Submit</button>
+          <span 
+            onClick={() => setMute(!mute)}
+            style={{width:'50px', height: '50px', backgroundColor: 'white'}}
+          >
+            {mute && 
+              <img 
+              src={off_mic}
+              alt="toggle text to speech" 
+              style={{width: '30px', height: '30px'}}/>
+            }
+            {!mute &&
+              <img
+              src={on_mic}
+              alt="toggle text to speech" 
+              style={{width: '30px', height: '30px'}}
+              />
+            }
+
+          </span>
         </form>
         {botResponse}
       </header>
